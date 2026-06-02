3ab6ac3b2942286d01bf4e7a1c4a8f912949c4ff
Bruce Becker <brucellino@protonmail.ch>
2026-06-02 09:55:08 +0200

    style: add egi colors
    Signed-off-by: Bruce Becker <brucellino@protonmail.ch>

--- src/js/vendor/highlight-entry.js
+++ src/js/vendor/highlight-entry.js
@@ -1,5 +1,4 @@
 /* Highlight.js browser bundle entry point */
 // Import the full highlight.js library with all languages pre-registered
 const hljs = require('highlight.js')
-
 module.exports = hljs
